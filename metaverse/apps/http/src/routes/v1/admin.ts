dbdeda3fba743c70ef296cb339e38b2fbdfe7c94
unknown <ajaypratap.singh@gmail.com>
2024-12-01 13:55:33 +0530

    first commit

---
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+adminRouter.post("/element", (req, res) => {});
+
+adminRouter.put("/element/:id", (req, res) => {});
+
+adminRouter.post("/avatar", (req, res) => {});
---
+++ metaverse/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+export const userRouter = Router();
+
+userRouter.post("/metadata", (req, res) => {});
+
+userRouter.get("/avatars", (req, res) => {});
+
+userRouter.get("/metadata", (req, res) => {});
