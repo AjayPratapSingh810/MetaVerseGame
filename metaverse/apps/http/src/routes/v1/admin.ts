e0743432af816d367a971a960ac2e7f726d5c9d1
ajaypratapsinghgeeky <pratapaj810@gmail.com>
2024-12-04 16:42:00 +0530

    apis of http server

--- metaverse/apps/http/src/routes/v1/admin.ts
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -1,9 +1,93 @@
 import { Router } from "express";
-
+import {
+  CreateAvatarSchema,
+  CreateElementSchema,
+  CreateMapSchema,
+  UpdateElementSchema,
+} from "../../types";
+import { adminMiddleware } from "../../middleware/admin";
+import client from "@repo/db/client";
 export const adminRouter = Router();
 
-adminRouter.post("/element", (req, res) => {});
+adminRouter.post("/element", adminMiddleware, (req, res) => {
+  const parsedData = CreateElementSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Validation failed" });
+    return;
+  }
 
-adminRouter.put("/element/:id", (req, res) => {});
+  const element = await client.element.create({
+    data: {
+      imageUrl: parsedData.data.imageUrl,
+      width: parsedData.data.width,
+      height: parsedData.data.height,
+      static: parsedData.data.static,
+    },
+  });
+  res.json({
+    id: element.id,
+  });
+});
 
-adminRouter.post("/avatar", (req, res) => {});
+adminRouter.put("/element/:id", adminMiddleware, async (req, res) => {
+  const parsedData = UpdateElementSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Validation failed" });
+    return;
+  }
+  const element = await client.element.findFirst({
+    where: { id: req.params.id },
+  });
+  if (!element) {
+    res.status(404).json({ message: "Element not found" });
+    return;
+  }
+  await client.element.update({
+    where: { id: req.params.id },
+    data: {
+      imageUrl: parsedData.data.imageUrl,
+    },
+  });
+  res.json({ message: "Update element successfully" });
+});
+
+adminRouter.post("/avatar", adminMiddleware,async (req, res) => {
+  const parsedData = CreateAvatarSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Validation failed" });
+    return;
+  }
+  const avatar = await client.avatar.create({
+    data: {
+      imageUrl: parsedData.data.imageUrl,
+      name: parsedData.data.name,
+    }
+  })
+  res.json({ avatarId: avatar.id });
+});
+
+adminRouter.post("/map", adminMiddleware, async(req, res) => {
+  const parsedData = CreateMapSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Validation failed" });
+    return;
+  }
+  const map = await client.map.create({
+    data: {
+      name: parsedData.data.name,
+      width: parseInt(parsedData.data.dimensions.split("x")[0]),
+      height: parseInt(parsedData.data.dimensions.split("x")[1]),
+      thumbnail: parsedData.data.thumbnail,
+      mapElements: {
+        create: parsedData.data.defaultElements.map(e => ({
+          elementId: e.elementId,
+            x: e.x,
+            y: e.y
+        }))
+      }
+    }
+  })
+  res.json({
+    id: map.id
+  })
+})
--- metaverse/apps/http/src/routes/v1/space.ts
+++ metaverse/apps/http/src/routes/v1/space.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { CreateElementSchema, CreateSpaceSchema } from "../../types";
+import {
+  AddElementSchema,
+  CreateElementSchema,
+  CreateSpaceSchema,
+  DeleteElementSchema,
+} from "../../types";
 import client from "@repo/db/client";
 import { userMiddleware } from "../../middleware/user";
 export const spaceRouter = Router();
@@ -85,21 +90,103 @@ spaceRouter.get("/all", userMiddleware, async (req, res) => {
   });
 });
 spaceRouter.post("/element", userMiddleware, async (req, res) => {
-  const parsedData = CreateElementSchema.safeParse(req.body);
+  const parsedData = AddElementSchema.safeParse(req.body);
   if (!parsedData.success) {
     res.status(400).json({ message: "Validation failed" });
     return;
   }
-  const element = await client.element.create({
-    data: {
-      imageUrl: parsedData.data.imageUrl,
-      width: parsedData.data.width,
-      height: parsedData.data.height,
-      static: parsedData.data.static,
+  const space = await client.space.findUnique({
+    where: {
+      id: parsedData.data.spaceId,
+      creatorId: req.userId,
     },
+    select: {
+      width: true,
+      height: true,
+    },
+  });
+  if (!space) {
+    res.status(403).json({ message: "Unauthorized" });
+    return;
+  }
+  await client.spaceElements.create({
+    data: {
+      spaceId: parsedData.data.spaceId,
+      elementId: parsedData.data.elementId,
+      x: parsedData.data.x,
+      y: parsedData.data.y,
+    },
+  });
+  res.json({ message: "Element added successfully" });
+});
+
+spaceRouter.get("/:spaceId", async (req, res) => {
+  const space = await client.space.findUnique({
+    where: { id: req.params.spaceId },
+    select: {
+      width: true,
+      height: true,
+      creatorId: true,
+      spaceElements: {
+        select: {
+          id: true,
+          element: {
+            select: {
+              id: true,
+              imageUrl: true,
+              width: true,
+              height: true,
+            },
+          },
+          x: true,
+          y: true,
+        },
+      },
+    },
+  });
+  if (!space) {
+    res.status(404).json({ message: "Space not found" });
+    return;
+  }
+  res.json({
+    dimensions: `${space.width}x${space.height}`,
+    elements: space.spaceElements.map((spaceElem) => {
+      return {
+        id: spaceElem.id,
+        element: {
+          id: spaceElem.element.id,
+          imageUrl: spaceElem.element.imageUrl,
+          width: spaceElem.element.width,
+          height: spaceElem.element.height,
+        },
+        x: spaceElem.x,
+        y: spaceElem.y,
+      };
+    }),
   });
 });
 
-spaceRouter.get("/:spaceId", (req, res) => {});
-
-spaceRouter.delete("/element", (req, res) => {});
+spaceRouter.delete("/element", userMiddleware, async (req, res) => {
+  const parsedData = DeleteElementSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({ message: "Validation failed" });
+    return;
+  }
+  const spaceElement = await client.spaceElements.findFirst({
+    where: { id: parsedData.data.id },
+    include: { space: true },
+  });
+  if (!spaceElement) {
+    res.status(404).json({ message: "Element not found" });
+    return;
+  }
+  if (
+    !spaceElement.space.creatorId ||
+    spaceElement.space.creatorId !== req.userId
+  ) {
+    res.status(403).json({ message: "Unauthorized" });
+    return;
+  }
+  await client.spaceElements.delete({ where: { id: parsedData.data.id } });
+  res.json({ message: "Element deleted" });
+});
